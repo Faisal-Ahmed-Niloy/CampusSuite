027d89da322277c01fcb5e8bc17d37fbc4c5d7e0
Faisal-Ahmed-Niloy <bd68454@gmail.com>
2026-01-19 16:59:17 +0600

    campusSuite: updated css

--- views/student/student_forgotPass.php
+++ views/student/student_forgotPass.php
@@ -16,6 +16,7 @@ session_start();
             <h2>Reset your password</h2>
             <label for="reset_pass">Enter your E-mail for password reset link:</label>
             <input type="email" name="reset_email" id="reset_email" placeholder="Enter your account's email address">
+            <input type="submit" value="submit">
             <p>Go <a href="student_login.php">back</a></p>
         </form>
     </div>
