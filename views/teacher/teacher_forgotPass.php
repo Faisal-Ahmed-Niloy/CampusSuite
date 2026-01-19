6c29d7e12d50935c8ea06f555170b29a486c284b
Its-Zenin <zeninjarir09@gmail.com>
2026-01-19 14:13:18 +0600

    campusSuite: basic ui setup  for teacher (login , logout , register , change pass, css)

---
+++ views/teacher/teacher_forgotPass.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher Forgot Password</title>
+    <link rel="stylesheet" href="teacher.css">
+
+</head>
+<body>
+
+        <div class="reg-container">
+            
+        <div class="form-boxforgotpass" id="forgot-password-form">
+    <form action="" method="POST">
+        <h2>Forgot Password</h2>
+        <label>Email</label><br> <br>
+        <input type="email/text" name="forgot_email" placeholder="Enter email or Number" required>
+
+        <button type="submit" name="forgot_password">Send Reset Link</button><br> <br>
+        <p>Remember your password? <a href="teacher_login.php">Login</a></p>
+    </form>
+</div>
+
+
+        </div>
+
+<body>
+</html>
---
+++ views/teacher/teacher_login.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher Login</title>
+    <link rel="stylesheet" href="teacher.css">
+
+</head>
+<body>
+
+        <div class=login-container>
+            <div class="form-boxlogin" id="login-form">
+                <form action="">
+                    <h2> Login </h2> <br> <br>
+                   
+                   <label>Email</label><br><br>
+                    <input type="email" name="log_email" placeholder="Email Address" required><br><br>
+                    <label>Password</label><br><br>
+                    <input type="password" name="log_password" placeholder="Password" required><br><br>
+                    <button type="submit" name="login">Login</button> <br> <br>
+                    <p> <a href="teacher_forgotPass.php"> Forgot Password? </a> </p> <br>
+                    <p> Don't have an account? <a href="teacher_register.php">Register/Sign Up</a> </p>
+                    
+                </form>
+            </div>
+
+
+
+
+        </div>
+
+<body>
+</html>
